e54c8ba31e0d7703f7b1548612167840c17c1f70
theanh <theanh@dev2.edunet24.de>
2018-09-19 13:41:55 +0200

    create backend server and reactapp

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://anhtran:deutsch123@ds261072.mlab.com:61072/devmerntta226",
+  secretOrKey: 'secret'
+};
---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const morgan = require("morgan");
+
+const users = require("./routes/api/users");
+const passport = require("passport");
+
+const app = express();
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+//DB config
+const db = require("./config/keys").mongoURI;
+
+//connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log("MongoDb connected");
+  })
+  .catch(err => console.log(err));
+
+// Passport middleware
+
+app.use(passport.initialize());
+
+//Passport Config
+require("./config/passport")(passport);
+
+// Use Routes
+app.use("/api/users", users);
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server running on port ---- ${port}`));
