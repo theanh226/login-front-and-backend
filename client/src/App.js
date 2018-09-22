0a9614b226cd5baa48cb4c80cbeafdc8c9e6d477
theanh <theanh@dev2.edunet24.de>
2018-09-22 13:26:41 +0200

    create Redux store

--- client/src/App.js
+++ client/src/App.js
@@ -7,21 +7,25 @@ import "./App.css";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Successs from "./components/response/Success";
+import { Provider } from "react-redux";
+import store from "./store";
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Navbar />
-          <Route exact path="/" component={Landing} />
-          <div className="container">
-          <Route exact path="/login" component={ Login } />
-          <Route exact path="/register" component={ Register } />
-          <Route exact path="/success" component={ Successs } />
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+              <Route exact path="/success" component={Successs} />
+            </div>
+            <Footer />
           </div>
-          <Footer />
-        </div>
-      </Router>
+        </Router>
+      </Provider>
     );
   }
 }
--- client/src/components/response/Success.js
+++ client/src/components/response/Success.js
@@ -5,11 +5,11 @@ class Successs extends Component {
   render() {
     return (
       <div className="container">
-        <h3 className="text-success text-center mb-4">
+        <h3 className="text-info text-center mb-4">
           The account has been initialized successfully
         </h3>
         <div class="d-flex justify-content-center mt-3">
-          <Link to="/login" className="btn btn-lg btn-success text-white">
+          <Link to="/login" className="btn btn-lg btn-info text-white">
             Go to Login
           </Link>
         </div>
